9750df79a5098ec961ddfc0e448588cf82281461
Andre Nogueira <aluiz.nogueira@gmail.com>
2016-01-07 16:38:20 -0200

    asfasddf

--- target/hello-angular/scripts/services/CustomerFactory.js
+++
@@ -1,4 +0,0 @@
-angular.module('helloangular').factory('CustomerResource', function($resource){
-    var resource = $resource('rest/customers/:CustomerId',{CustomerId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
-    return resource;
-});
